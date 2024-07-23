d702e6cc375ad0211a7f9b83c640eec612dbe4fa
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-07-22 20:54:26 -0500

    logger

--- lambda_function.py
+++ lambda_function.py
@@ -32,20 +32,21 @@ def lambda_handler(event, context):
     aws_secret_access_key=key
     region_name='us-east-1'
 
-    logger.info("aws_access_key_id --> ",aws_access_key_id)
-    logger.info("aws_secret_access_key --> ", aws_secret_access_key)
-
-    # Create an S3 client
+    logger.info("Creating session")
     session = boto3.Session(aws_access_key_id=aws_access_key_id,
                             aws_secret_access_key=aws_secret_access_key,
                             region_name=region_name)
+    
+    logger.info("Creating S3 client")
     s3 = session.client('s3')
     
     
-    print("Call S3 to list current buckets")
+    logger.info("Listing buckets")
     response = s3.list_buckets()
     
-    print("response: ", response)
+    logger.info(response)
+
+    logger.info("Printing bucket list")
     buckets = [bucket['Name'] for bucket in response['Buckets']]
 
     # Print out the bucket list
