af9fd559e988c4d9bfa5dcc8ae58172b1f810df2
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-07-22 21:23:43 -0500

    simple

--- lambda_function.py
+++ lambda_function.py
@@ -20,36 +20,15 @@ def lambda_handler(event, context):
     logger.info("Testing 2")
     logger.info(os.environ)
 
-    
-    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"]
-    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
-    region_name='us-east-1'
 
-    logger.info("Creating session")
-    session = boto3.Session(aws_access_key_id=aws_access_key_id,
-                            aws_secret_access_key=aws_secret_access_key,
-                            region_name=region_name)
-    
-    logger.info("Creating S3 client")
-    s3 = session.client('s3')
-    
-    
-    logger.info("Listing buckets")
-    response = s3.list_buckets()
-    
-    logger.info(response)
+    logger.info("create s3 client")
+    s3_client = boto3.client('s3')
 
-    logger.info("Printing bucket list")
-    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    logger.info("get list of buckets")
+    bucket_list = s3_client.list_buckets()
+    logger.info(bucket_list)
 
-    # Print out the bucket list
-    print("Bucket List: %s" % buckets)
-    # print bucket owner
-    print("bucket owner: ", response['Owner'])
-    # print bucket count
-    print("bucket count: ", len(buckets))
-    # print bucket names
-    print("bucket names: ", buckets)
+    
 
     return {
         'statusCode': 200,
