fcce2b5d10a19b5f532889cc1944eaaf3180fd03
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-07-28 13:54:54 -0500

    haha

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,6 @@ logger.setLevel(logging.INFO)
 
 from creds import get_access_key_id, get_secret_access_key
 
-
 #load_dotenv()
 
 def lambda_handler(event, context):
@@ -53,6 +52,7 @@ def lambda_handler(event, context):
 
     return {
         # return buckets JSON object
-        buckets: buckets,
-        message: json.dumps(message)
+        "message": message,
+        "buckets": json.dumps(buckets)
+
     }
