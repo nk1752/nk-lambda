54dc53349a131289a76e2bf698958955e7f32dd9
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-07-28 10:29:07 -0500

    return buckets

--- lambda_function.py
+++ lambda_function.py
@@ -53,8 +53,8 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         # add a header to the response
-        #'headers': {'Content-Type': 'application/json'},
+        'headers': {'Content-Type': 'application/json'},
         # return bucket_list as JSON
-        #'body': json.dumps(buckets)
-        'message': message
+        'body': json.dumps(buckets)
+        #'message': message
     }
